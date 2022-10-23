9d93748b01e2af1b831562ee3674c5ab39b7ad12
Ferenc Péli <ferenc.peli@axzoom.de>
2022-10-23 10:51:08 +0200

    Refactoring global-setup

--- global-setup.ts
+++ global-setup.ts
@@ -1,4 +1,4 @@
-import { chromium, FullConfig } from "@playwright/test";
+import { chromium, FullConfig, Page } from "@playwright/test";
 
 async function globalSetup(config: FullConfig) {
 	const browser = await chromium.launch();
@@ -6,28 +6,25 @@ async function globalSetup(config: FullConfig) {
 	// admin
 	const adminPage = await browser.newPage();
 	await adminPage.goto(config.projects[0].use.baseURL!);
-	await adminPage.fill("input[type=email]", process.env.ADMIN_USERNAME!);
-	await adminPage.click("input[type=submit]");
-	await adminPage.fill("input[type=password]", process.env.ADMIN_PASSWORD!);
-	await adminPage.click("input[type=submit]");
-	await adminPage.waitForSelector(".identity");
-	await adminPage.click("input[type=submit]");
-	await adminPage.waitForSelector(".p-card-body");
-	await adminPage.context().storageState({ path: "temp/adminStorageState.json" });
+	await storeAuthenticationState(adminPage, process.env.ADMIN_USERNAME!, process.env.ADMIN_PASSWORD!, "admin");
 
 	// employee
 	const employeePage = await browser.newPage();
 	await employeePage.goto(config.projects[0].use.baseURL!);
-	await employeePage.fill("input[type=email]", process.env.EMPLOYEE_USERNAME!);
-	await employeePage.click("input[type=submit]");
-	await employeePage.fill("input[type=password]", process.env.EMPLOYEE_PASSWORD!);
-	await employeePage.click("input[type=submit]");
-	await employeePage.waitForSelector(".identity");
-	await employeePage.click("input[type=submit]");
-	await employeePage.waitForSelector(".p-card-body");
-	await employeePage.context().storageState({ path: "temp/employeeStorageState.json" });
+	await storeAuthenticationState(employeePage, process.env.EMPLOYEE_USERNAME!, process.env.EMPLOYEE_PASSWORD!, "employee");
 
 	await browser.close();
+}
+
+async function storeAuthenticationState(page: Page, username: string, password: string, type: string) {
+	await page.fill("input[type=email]", username);
+	await page.click("input[type=submit]");
+	await page.fill("input[type=password]", password);
+	await page.click("input[type=submit]");
+	await page.waitForSelector(".identity");
+	await page.click("input[type=submit]");
+	await page.waitForSelector(".p-card-body");
+	await page.context().storageState({ path: `temp/${type}StorageState.json` });
 }
 
 export default globalSetup;
